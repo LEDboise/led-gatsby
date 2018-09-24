20c56138671213387b769f55a17f50452bb8489c
Kyle Morck <kyle@ledboise.com>
2018-09-23 20:04:35 -0700

    Added about page

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,8 +7,6 @@ import Layout from '../components/layout'
 const IndexPage = ({ data }) => (
   <Layout>
     <h1>Hello people</h1>
-    <p>Welcomey delcomey to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
     <div dangerouslySetInnerHTML={{ __html: data.datoCmsHomePage.introduction }} />
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import { graphql } from 'gatsby'
+
 
 import Layout from '../components/layout'
 
-const SecondPage = () => (
+const SecondPage = ({ data }) => (
   <Layout>
     <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <div dangerouslySetInnerHTML={{ __html: data.datoCmsAboutPage.aboutUs }} />
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
+
+export const query = graphql`
+  query AboutQuery {
+    datoCmsAboutPage {
+      aboutUs
+    }
+  }
+`;
 
 export default SecondPage
