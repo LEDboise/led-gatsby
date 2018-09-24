b8af0e78ce57332eb7d75394d5dce062067e4115
Kyle Morck <kyle@ledboise.com>
2018-09-23 19:54:59 -0700

    Added DatoCMS source

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,13 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
+    {
+      resolve: `gatsby-source-datocms`,
+      options: {
+        apiToken: `591d1a5fbb1caf7debba91a97b6e2f`,
+        preview: false,
+        disableLiveReload: false,
+      },
+    },
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import { graphql } from 'gatsby'
 
 import Layout from '../components/layout'
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <h1>Hello people</h1>
     <p>Welcomey delcomey to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+    <div dangerouslySetInnerHTML={{ __html: data.datoCmsHomePage.introduction }} />
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
+
+export const query = graphql`
+  query HomeQuery {
+    datoCmsHomePage {
+      introduction
+    }
+  }
+`;
 
 export default IndexPage
